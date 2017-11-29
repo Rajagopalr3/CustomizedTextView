61793a3a938d5a350a5967c19cc92ee322a0d14a
Rajagopal92 <raja.g@invenzolabs.com>
2017-11-29 16:57:29 +0530

    update checking

--- app/src/main/java/com/raja/customtextview/MainActivity.java
+++ app/src/main/java/com/raja/customtextview/MainActivity.java
@@ -18,9 +18,6 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
