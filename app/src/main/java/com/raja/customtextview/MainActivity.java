a156c439d1da846d6136df613e3096342ed86b2d
Rajagopal92 <raja.g@invenzolabs.com>
2017-11-29 16:55:06 +0530

    update checking

--- app/src/main/java/com/raja/customtextview/MainActivity.java
+++ app/src/main/java/com/raja/customtextview/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Raja", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
